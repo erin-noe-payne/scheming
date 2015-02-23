f7905da3e141817aeae03f3f60a9a27df33bb761
autoric <saizul@gmail.com>
2015-02-23 13:49:56 -0700

    Version 1.0.0

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -258,6 +258,25 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               ]
             }
           ]
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 1,
+            "title": "Changelog",
+            "slug": "changelog"
+          },
+          "depth": 1,
+          "children": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "v1.0.0",
+                "slug": "v100"
+              },
+              "depth": 2
+            }
+          ]
         }
       ]
     }, {
